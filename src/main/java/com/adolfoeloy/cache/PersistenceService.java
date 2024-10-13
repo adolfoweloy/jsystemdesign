d6caee8c0f715febccdc300a63fe11710564fb9c
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2024-10-13 16:22:16 +1100

    writing some other examples of caching writing strategies

--- src/main/java/com/adolfoeloy/cache/PersistenceService.java
+++ src/main/java/com/adolfoeloy/cache/PersistenceService.java
@@ -1,9 +1,14 @@
 package com.adolfoeloy.cache;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class PersistenceService {
 
     private final Cache caching = new Cache();
     private final Database database = new Database();
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     /**
      * Write to both the cache and to the database.
@@ -13,6 +18,27 @@ public class PersistenceService {
     public void writeThrough(String key, String value) {
         caching.write(key, value);
         database.write(key, value);
+    }
+
+    /**
+     * Write directly and only to the database. When using this strategy, the cache is usually updated
+     * on cache-misses when reading from the cache.
+     * @see this.read
+     */
+    public void writeAround(String key, String value) {
+        database.write(key, value);
+    }
+
+    /**
+     * Write first to the cache and asynchronously to the database.
+     */
+    public void writeBack(String key, String value) {
+        caching.write(key, value);
+
+        // writes to the database is performed asynchronously unblocking the write operation as a whole
+        executorService.submit(() -> {
+            database.write(key, value);
+        });
     }
 
     public String read(String key) {
